9245744642dbf72eb6fa8888cefc7cc9dd8f2b07
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-06-07 20:00:14 -0400

    First commit

---
+++ HeadStartCh4Exercice/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace HeadStartCh4Exercice;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Press 1 for Lloyd, 2 for Lucinda, 3 to swap: ");
+        var input = Console.ReadLine();
+        var IntInput = Convert.ToInt32(input);
+
+        var lucinda = new Elephant() { name = "Lucinda", EarSize = 33 };
+        var lloyd = new Elephant() { name = "Lloyd", EarSize = 40 };
+
+        if (IntInput == 1)
+        {
+            Console.WriteLine("Calling lloyd.WhoAmI()");
+            lloyd.WhoAmI();
+            
+
+        }
+        else if(IntInput == 2)
+        {
+            Console.WriteLine("Calling lucinda.WhoAmI()");
+            lucinda.WhoAmI();
+            
+        }else if (IntInput == 3)
+        {
+            Console.WriteLine("Reference variables have been swapped.");
+
+            Elephant Holder;
+            Holder = lloyd;
+            lucinda = Holder;
+            lucinda.WhoAmI();
+            lloyd.WhoAmI();
+        }
+
+        Console.ReadLine();
+    }
+    
+    
+}
+
+public class Elephant
+{
+
+    public string name;
+    public int EarSize;
+
+    public void WhoAmI()
+    {
+        Console.WriteLine($"My name is {name}");
+        Console.WriteLine($"My ears are {EarSize} inches tall.");
+
+        
+    }
+
+    
+
+
+
+
+
+}
+
